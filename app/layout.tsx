7917f91b0a5dbae9f1721cb8f40590937b969bda
EnricCoding <enricrbt@gmail.com>
2024-11-27 13:55:32 +0100

    Remove default next.js font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { genPageMetadata } from "./genMetadata";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = genPageMetadata({
   title: "Jose GhostWriting - Potencia tu marca persona",
@@ -29,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` antialiased`}
       >
         <main>
           {children}
